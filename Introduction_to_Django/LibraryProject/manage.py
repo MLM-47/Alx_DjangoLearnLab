c694c3a047f5810206cb9e215f89101fd90c1707
Levix <mikelevix@gmail.com>
2025-09-22 16:17:53 +0300

    added commit

--- Introduction_to_Django/LibraryProject/manage.py
+++ Introduction_to_Django/LibraryProject/manage.py
@@ -20,3 +20,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+from bookshelf.models import Book
+
+# Create
+book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
+book
