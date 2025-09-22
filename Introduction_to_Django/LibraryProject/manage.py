d8695ab4e9e517974874fda8140804a575576d9c
Levix <mikelevix@gmail.com>
2025-09-22 16:30:08 +0300

    added crud opeartions

--- Introduction_to_Django/LibraryProject/manage.py
+++ Introduction_to_Django/LibraryProject/manage.py
@@ -21,8 +21,31 @@ def main():
 if __name__ == '__main__':
     main()
 
+#CREATE
 from bookshelf.models import Book
-
-# Create
 book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
 book
+# Expected Output: <Book: 1984 by George Orwell (1949)>
+
+#RETRIEVE
+Book.objects.all()
+# Expected Output: <QuerySet [<Book: 1984 by George Orwell (1949)>]>
+
+book = Book.objects.get(title="1984")
+book.title, book.author, book.publication_year
+# Expected Output: ('1984', 'George Orwell', 1949)
+
+#UPDATE
+book = Book.objects.get(title="1984")
+book.title = "Nineteen Eighty-Four"
+book.save()
+book
+# Expected Output: <Book: Nineteen Eighty-Four by George Orwell (1949)>
+
+#DELETE
+book = Book.objects.get(title="Nineteen Eighty-Four")
+book.delete()
+# Expected Output: (1, {'bookshelf.Book': 1})
+
+Book.objects.all()
+# Expected Output: <QuerySet []>
