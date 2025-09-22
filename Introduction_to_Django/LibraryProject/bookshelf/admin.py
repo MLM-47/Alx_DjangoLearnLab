798e2a93520072264079b66304ed08c578a91a15
Levix <mikelevix@gmail.com>
2025-09-22 20:57:12 +0300

    added book model to admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year', 'created_at')  
+    search_fields = ('title',)                               
+    list_filter = ('title',)                           
+    ordering = ('-created_at',)
+    
+admin.site.register(Book)
